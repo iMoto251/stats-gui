17609c9e9a248ae27f280c5128360276023da67c
iMoto251 <55514345+iMoto251@users.noreply.github.com>
2023-01-10 20:08:14 -0600

    Added MX Overall Function

--- main.js
+++ main.js
@@ -1058,6 +1058,225 @@ async function tcOverall(urlm1, urlm2, urlm3){
 
 }
 
+async function mxOveralls(urlm1, urlm2, nation, series ){
+    let browser = await puppeteer.launch({headless: true});
+    let page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080})
+    await page.setDefaultNavigationTimeout(120000);
+    await page.goto(urlm1);
+    await page.waitForNetworkIdle();
+
+    let resultsm1 = await page.evaluate(() =>{
+        function capitalize(str) {
+            return str.replace(
+                /\w\S*/g,
+                function(txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1);
+                }
+            );
+        }
+        function motoPoints(pos){
+            switch(pos){
+                case 1:
+                    return 25;
+                case 2:
+                    return 22;
+                case 3:
+                    return 20;
+                case 4:
+                    return 18;
+                case 5:
+                    return 16;
+                case 6:
+                    return 15;
+                case 7:
+                    return 14;
+                case 8:
+                    return 13;
+                case 9:
+                    return 12;
+                case 10:
+                    return 11;
+                case 11:
+                    return 10;
+                case 12:
+                    return 9;
+                case 13:
+                    return 8;
+                case 14:
+                    return 7;
+                case 15:
+                    return 6;
+                case 16:
+                    return 5;
+                case 17:
+                    return 4;
+                case 18:
+                    return 3;
+                case 19:
+                    return 2;
+                case 20:
+                    return 1;
+                default:
+                    return 0;
+            }
+
+        }
+
+        let position = document.querySelectorAll(`td.pos`);
+        let moto1 = [];
+        let posNum = position.length;
+        for(let i=0;i<position.length;i++){
+            moto1[i] = {
+                position: parseInt(document.querySelector(`body > div.main > table:nth-child(5) > tbody > tr:nth-child(${i+2}) > td.pos`).innerHTML),
+                number: document.querySelector(`table.laptimes:nth-child(5) > tbody:nth-child(1) > tr:nth-child(${i+2}) > td:nth-child(2)`).innerHTML,
+                name: capitalize(document.querySelector(`table.laptimes:nth-child(5) > tbody:nth-child(1) > tr:nth-child(${i+2}) > td:nth-child(3) > a:nth-child(1)`).innerHTML),
+                uid: parseInt(document.querySelector(`table.laptimes:nth-child(5) > tbody:nth-child(1) > tr:nth-child(${i+2}) > td:nth-child(9)`).innerHTML),
+                points: motoPoints(i+1)
+            }
+
+        }
+        return {posNum, moto1};
+    });
+
+    await page.goto(urlm2);
+    await page.waitForNetworkIdle();
+
+    let resultsm2 = await page.evaluate(() =>{
+        function capitalize(str) {
+            return str.replace(
+                /\w\S*/g,
+                function(txt) {
+                    return txt.charAt(0).toUpperCase() + txt.substr(1);
+                }
+            );
+        }
+        function motoPoints(pos){
+            switch(pos){
+                case 1:
+                    return 25;
+                case 2:
+                    return 22;
+                case 3:
+                    return 20;
+                case 4:
+                    return 18;
+                case 5:
+                    return 16;
+                case 6:
+                    return 15;
+                case 7:
+                    return 14;
+                case 8:
+                    return 13;
+                case 9:
+                    return 12;
+                case 10:
+                    return 11;
+                case 11:
+                    return 10;
+                case 12:
+                    return 9;
+                case 13:
+                    return 8;
+                case 14:
+                    return 7;
+                case 15:
+                    return 6;
+                case 16:
+                    return 5;
+                case 17:
+                    return 4;
+                case 18:
+                    return 3;
+                case 19:
+                    return 2;
+                case 20:
+                    return 1;
+                default:
+                    return 0;
+            }
+
+        }
+
+        let position = document.querySelectorAll(`td.pos`);
+        let moto2 = [];
+        let posNum = position.length;
+        for(let i=0;i<position.length;i++){
+            moto2[i] = {
+                position: parseInt(document.querySelector(`body > div.main > table:nth-child(5) > tbody > tr:nth-child(${i+2}) > td.pos`).innerHTML),
+                number: document.querySelector(`table.laptimes:nth-child(5) > tbody:nth-child(1) > tr:nth-child(${i+2}) > td:nth-child(2)`).innerHTML,
+                name: capitalize(document.querySelector(`table.laptimes:nth-child(5) > tbody:nth-child(1) > tr:nth-child(${i+2}) > td:nth-child(3) > a:nth-child(1)`).innerHTML),
+                uid: parseInt(document.querySelector(`table.laptimes:nth-child(5) > tbody:nth-child(1) > tr:nth-child(${i+2}) > td:nth-child(9)`).innerHTML),
+                points: motoPoints(i+1)
+            }
+
+        }
+        return {posNum, moto2};
+    });
+
+    let overall = [];
+    for(let i=0;i<resultsm1.posNum;i++){
+        overall[i] = {
+            name: resultsm1.moto1[i].name,
+            uid: resultsm1.moto1[i].uid,
+            number: resultsm1.moto1[i].number,
+            moto1: resultsm1.moto1[i].position,
+            moto2: "DNS",
+            points: (0 - parseInt(resultsm1.moto1[i].position) - 60),
+            overallPos: null
+        }
+        for(let j=0;j<resultsm2.posNum;j++){
+            if(resultsm2.moto2[j].uid === resultsm1.moto1[i].uid){
+                if((resultsm1.moto1[i].points + resultsm2.moto2[j].points) > 0){
+                    overall[i] = {
+                        name:resultsm1.moto1[i].name,
+                        uid: resultsm1.moto1[i].uid,
+                        number:resultsm1.moto1[i].number,
+                        moto1:resultsm1.moto1[i].position,
+                        moto2:resultsm2.moto2[j].position,
+                        points:(resultsm1.moto1[i].points + resultsm2.moto2[j].points),
+                        overallPos: null
+                    }
+                } else {
+                    overall[i] = {
+                        name:resultsm1.moto1[i].name,
+                        uid: resultsm1.moto1[i].uid,
+                        number:resultsm1.moto1[i].number,
+                        moto1:resultsm1.moto1[i].position,
+                        moto2:resultsm2.moto2[j].position,
+                        points:(0 - parseInt(resultsm1.moto1[i].position) - parseInt(resultsm2.moto2[j].position)),
+                        overallPos: null
+                    }
+                }
+            }
+        }
+    }
+    overall.sort((a,b)=>(a.points < b.points) ? 1 : -1)
+
+    for(let i = 0;i<overall.length;i++){
+        let name = '';
+        let team = '';
+        let bikeColor = '000000';
+        for(let b=0;b<teams.length;b++){
+            if(overall[a].uid === parseInt(teams[b].uid)){
+                bikeColor = teams[b].bike;
+                team = teams[b].team;
+                name = teams[b].name;
+            } else {
+                //do nothing
+            }
+        }
+
+        if(team === "Privateer"){
+            fs.appendFileSync(`${path.join(__dirname, "stats.txt")}`, `${i+1}. [i][size=85]#${overall[i].number}[/size][/i] - ${name} [i][size=85](${overall[i].moto1} - ${overall[i].moto2})[/size][/i]\n`)
+        } else {
+            fs.appendFileSync(`${path.join(__dirname, "stats.txt")}`, `${a+1}. [i][size=85]#${overall[i].number}[/size][/i] - ${name} | [size=85][color=#${bikeColor}]${team}[/color][/size] [i][size=85](${overall[i].moto1} - ${overall[i].moto2})[/size][/i]\n`)
+        }
+    }
+    await browser.close();
+}
+
 async function doStats(){
     var count = Object.keys(stats).length;
 
